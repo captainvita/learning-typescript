b12378e48c2333f2b7865df43272d2558e9bd30b
captainvita <vitalikifel@gmx.de>
2026-03-12 21:05:30 +0800

    Update odd-even-numbers.ts
    Break code into functions:
    - generateEvenOddNumbers()
    - filterOddEvenNumbers()
    - filterTimestampNumbers()
    - logNumbers()
    - logWithStatus()

--- src/02-exercises/odd-even-numbers.ts
+++ src/02-exercises/odd-even-numbers.ts
@@ -3,55 +3,81 @@ const FIVE_MINUTES = 5 * 60 * 1000;
 let zeroToMax: number[] = [];
 
 for (let i = 0; i <= MAX_NUMBER; i++) {
-    zeroToMax.push(i);
+  zeroToMax.push(i);
 }
 
 let even: number[] = [];
 let odd: number[] = [];
 
 for (let v of zeroToMax) {
-    if (v % 2 === 0) {
-        even.push(v);
-    } else {
-        odd.push(v);
-    }
+  if (v % 2 === 0) {
+    even.push(v);
+  } else {
+    odd.push(v);
+  }
 }
 
 console.log(`Even Numbers: ${even.join(", ")}`);
 console.log(`Odd Numbers: ${odd.join(", ")}`);
 
 type OddOrEven = {
-    value: number;
-    isEven: boolean;
-    oddOrEvenKey: 0 | 1;
-    timestamp: number;
-}
+  value: number;
+  isEven: boolean;
+  oddOrEvenKey: 0 | 1;
+  timestamp: number;
+};
 
 const oddEvenStatus = {
-    0: "Even",
-    1: "Odd",
+  0: "Even",
+  1: "Odd",
+};
+
+function generateEvenOddNumbers(max: number): OddOrEven[] {
+  const nums: OddOrEven[] = [];
+  for (let i = 0; i <= max; i++) {
+    nums.push({
+      value: i,
+      isEven: i % 2 === 0,
+      oddOrEvenKey: i % 2 === 0 ? 0 : 1,
+      timestamp: Date.now(),
+    });
+  }
+  return nums;
 }
 
-let numbers: OddOrEven[] = [];
-
-for (let i = 0; i <= MAX_NUMBER; i++) {
-    numbers.push({
-        value: i,
-        isEven: i % 2 === 0,
-        oddOrEvenKey: i % 2 === 0 ? 0 : 1,
-        timestamp: Date.now(),
-    })
+function filterOddEvenNumbers(nums: OddOrEven[]): {
+  evenNumbers: OddOrEven[];
+  oddNumbers: OddOrEven[];
+} {
+  const evenNumbers: OddOrEven[] = nums.filter((e) => e.isEven === true);
+  const oddNumbers: OddOrEven[] = nums.filter((e) => e.isEven === false);
+  return { evenNumbers, oddNumbers };
 }
 
-const evenNumbers = numbers.filter(e => e.isEven === true);
-const oddNumbers = numbers.filter(e => e.isEven === false);
+function filterTimestampNumbers(nums: OddOrEven[]): OddOrEven[] {
+  const fiveMinNumbers = nums.filter(
+    (e) => e.timestamp >= Date.now() - FIVE_MINUTES,
+  );
+  return fiveMinNumbers;
+}
 
-const fiveMinNumbers = numbers.filter(e => e.timestamp >= Date.now() - FIVE_MINUTES)
+function logNumbers(nums: OddOrEven[]): void {
+  const { evenNumbers, oddNumbers } = filterOddEvenNumbers(nums);
+  console.log(`Even Numbers: ${evenNumbers.map((e) => e.value).join(", ")}`);
+  console.log(`Odd Numbers: ${oddNumbers.map((e) => e.value).join(", ")}`);
+  console.log(
+    `Numbers from last 5 Min.: ${filterTimestampNumbers(nums)
+      .map((e) => e.value)
+      .join(", ")}`,
+  );
+}
 
-console.log(`Even Numbers: ${evenNumbers.map(e => e.value).join(", ")}`)
-console.log(`Odd Numbers: ${oddNumbers.map(e => e.value).join(", ")}`);
-console.log(`Numbers from last 5 Min.: ${fiveMinNumbers.map(e => e.value).join(", ")}`);
-
-for (const k of numbers) {
+function logWithStatus(nums: OddOrEven[]): void {
+  for (const k of nums) {
     console.log(`Value ${k.value} is ${oddEvenStatus[k.oddOrEvenKey]}`);
+  }
 }
+
+const numbers: OddOrEven[] = generateEvenOddNumbers(MAX_NUMBER);
+logNumbers(numbers);
+logWithStatus(numbers);
