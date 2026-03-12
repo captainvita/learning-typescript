8086a9e9e826fc34a5214730c025c8a048f77524
captainvita <vitalikifel@gmx.de>
2026-03-12 12:37:06 +0800

    Added linked-list.ts in 02-exercises
    Created singly linked list with interface + type and functions to create
    node and list

---
+++ src/02-exercises/linked-list.ts
@@ -0,0 +1,32 @@
+/* Linked List is a linear data structure consisting if nodes that are connected.
+Each node has a data field and one or more pointers connecting to other nodes.
+The first node is the head and the last node the tail that is pointing to null.
+
+Singly Linked List:
+-----    -----
+|D|P| -> |D|P|
+-----    -----
+
+Doubly Linked List:
+-------     -------
+|P|D|P| <-> |P|D|P|
+-------     -------
+*/
+
+interface singleNode<T> {
+    value: T;
+    next: singleNode<T> | null;
+}
+
+type singlyLinkedList<T> = {
+    head: singleNode<T> | null;
+    size: number;
+}
+
+function createSingleNode<T> (value: T): singleNode<T> {
+    return {value, next: null};
+}
+
+function createSinglyLinkedList<T> (): singlyLinkedList<T> {
+    return {head: null, size: 0};
+}
